65c20c876fa767e7e47a36283b96247f8afe1124
binsgx <binsgx@gmail.com>
2015-10-09 16:29:53 +0700

    Add Setting

---
+++ ParkingSpace.Services/SettingService.cs
@@ -0,0 +1,32 @@
+﻿using ParkingSpace.Models;
+using ParkingSpace.Services.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParkingSpace.DataAccess.Core;
+
+namespace ParkingSpace.Services {
+  public class SettingService : ServiceBase<App, Setting> {
+    public override IRepository<Setting> Repository { get; set; }
+
+    public override Setting Find(params object[] keys) {
+      int id = (int)keys[0];
+      return Query(item => item.Id == id).SingleOrDefault();
+    }
+
+    public Setting Current {
+      get {
+        var setting = All().FirstOrDefault();
+        if (setting == null) {
+          setting = new Setting();
+          Add(setting);
+          SaveChanges();
+        }
+
+        return setting;
+      }
+    }
+  }
+}
